4a522f03ea32feb8560ac5f3e0db5d53b4e7c07c
distray <52904880+distray@users.noreply.github.com>
2022-06-21 00:43:14 -0500

    Create kb.lua

---
+++ scripts/kb.lua
@@ -0,0 +1,8 @@
+keyboard.KeyDown:connect(function(key)
+    if key == "Q, W, E, R, T, Y, U, I, O, P, A, S, D, F, G, H, J, K, L, Z, X, C, V, B, N, M, -, =, [, ], ;, ', ,, ., /, ?, :, |, \, `, ~" then
+        print("You have successfully clicked the key!")
+        end
+end)
+
+-- This is just a script I made when I was bored to see if all my keys were functioning.
+-- Does not work with " due to formatting.
